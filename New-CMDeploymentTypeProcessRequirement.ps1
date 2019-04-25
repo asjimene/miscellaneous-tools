d070a55150057c367b5775d9fe03cfbf549c64f6
Andrew Jimenez <asjimenez91@gmail.com>
2019-04-25 14:17:54 -0700

    Create New-CMDeploymentTypeProcessRequirement.ps1
    Adds a Deployment "Install Behavior" Item using an existing "Install Behavior" Item from a source Application.

---
+++ New-CMDeploymentTypeProcessRequirement.ps1
@@ -0,0 +1,49 @@
+Function New-CMDeploymentTypeProcessRequirement {
+    # Creates a Deployment Type Process Requirement by copying an existing Process Requirement.
+    # A Process requirement needs to be Defined in the "Install Behavior" Tab of the "SourceApplicationName" Variable before this script will function properly
+	Param (
+		[System.String]$SourceApplicationName,
+		[System.String]$DestApplicationName,
+		[System.String]$DestDeploymentTypeName,
+        [System.String]$ProcessRequirementDisplayName,
+        [System.String]$ProcessRequirementExecutable
+	)
+    Push-Location
+    Set-Location $SCCMSite
+	$DestDeploymentTypeIndex = 0
+ 
+    # get the applications
+    $SourceApplication = Get-CMApplication -Name $SourceApplicationName | ConvertTo-CMApplication
+    $DestApplication = Get-CMApplication -Name $DestApplicationName | ConvertTo-CMApplication
+	
+	# Get DestDeploymentTypeIndex by finding the Title
+	$DestApplication.DeploymentTypes | ForEach-Object {
+		$i = 0
+	} {
+		If ($_.Title -eq "$DestDeploymentTypeName") {
+			$DestDeploymentTypeIndex = $i
+			
+		}
+		$i++
+    }
+    
+    # Get requirement rules from source application
+    $ProcessRequirementsList = $SourceApplication.DeploymentTypes[0].Installer.InstallProcessDetection.ProcessList[0]
+    $ProcessRequirementsList
+    if (-not ([System.String]::IsNullOrEmpty($ProcessRequirementsList))) {
+        $ProcessRequirementsList.Name = $ProcessRequirementExecutable
+        $ProcessRequirementsList.DisplayInfo[0].DisplayName = $ProcessRequirementDisplayName
+        $ProcessRequirementsList
+        $DestApplication.DeploymentTypes[$DestDeploymentTypeIndex].Installer.InstallProcessDetection.ProcessList.Add($ProcessRequirementsList)
+    }
+ 
+    # push changes
+    $CMApplication = ConvertFrom-CMApplication -Application $DestApplication
+    $CMApplication.Put()
+    Pop-Location
+}
+
+$SCCMSite = "SITENAME:"
+Import-Module ConfigurationManager
+
+New-CMDeploymentTypeProcessRequirement -SourceApplicationName "SourceAppwithProcessRequirementdefined" -DestApplicationName "DestinationAppName" -DestDeploymentTypeName "DestinationDepType" -ProcessRequirementDisplayName "Microsoft Outlook" -ProcessRequirementExecutable "Outlook.exe"
