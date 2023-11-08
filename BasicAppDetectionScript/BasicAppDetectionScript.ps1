5bf6efdfe6ba94b8847539fc6fc602b6bd50ba52
Andrew Jimenez <3790176+asjimene@users.noreply.github.com>
2023-11-08 15:37:45 -0700

    Update Detection Script again

--- BasicAppDetectionScript/BasicAppDetectionScript.ps1
+++ BasicAppDetectionScript/BasicAppDetectionScript.ps1
@@ -1,11 +1,11 @@
 $AppName = "" # DisplayName in Add/Remove Programs
 $AppVersion = "" # DisplayVersion of the App in Add/Remove Programs
-$WindowsInstaller= "" # 1 or 0 | 1 is MSI 0 is EXE
-$SystemComponent= "" # 1 or 0 | 1 is SystemComponent = 1, 0 is SystemComponent does not exist or is 0
+$WindowsInstaller =  # 1 or 0 | 1 is MSI 0 is EXE
+$SystemComponent =  # 1 or 0 | 1 is SystemComponent = 1, 0 is SystemComponent does not exist or is 0
 
 # Gather all the apps in the Add/Remove Programs Registry Keys
-$Apps = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | select DisplayName, DisplayVersion, WindowsInstaller
-$Apps += (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | select DisplayName, DisplayVersion, WindowsInstaller
+$Apps = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | select DisplayName, DisplayVersion, WindowsInstaller, SystemComponent
+$Apps += (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | select DisplayName, DisplayVersion, WindowsInstaller, SystemComponent
 
 # Check is the App DisplayName is found and the version in the registry is greater than or equal to the specified AppVersion
 $AppFound = $Apps | Where-Object {
