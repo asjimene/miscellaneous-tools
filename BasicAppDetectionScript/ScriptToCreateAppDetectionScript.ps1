bbcf4b4b0c42296ebd637c7330f4bd0c10655db0
Andrew Jimenez <3790176+asjimene@users.noreply.github.com>
2023-11-08 16:12:58 -0700

    Add Script to Create the Detection Script

---
+++ BasicAppDetectionScript/ScriptToCreateAppDetectionScript.ps1
@@ -0,0 +1,32 @@
+$DetectionScript = @'
+$AppName = "@AppName" # DisplayName in Add/Remove Programs
+$AppVersion = "@AppVersion" # DisplayVersion of the App in Add/Remove Programs
+$WindowsInstaller = @WindowsInstaller # 1 or 0 | 1 is MSI 0 is EXE
+$SystemComponent = @SystemComponent # 1 or 0 | 1 is SystemComponent = 1, 0 is SystemComponent does not exist or is 0
+
+# Gather all the apps in the Add/Remove Programs Registry Keys
+$Apps = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | select DisplayName, DisplayVersion, WindowsInstaller, SystemComponent
+$Apps += (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | select DisplayName, DisplayVersion, WindowsInstaller, SystemComponent
+
+# Check is the App DisplayName is found and the version in the registry is greater than or equal to the specified AppVersion
+$AppFound = $Apps | Where-Object {
+	($_.DisplayName -like $AppName) -and ([version]$_.DisplayVersion -ge [version]$AppVersion) -and ([bool]$_.WindowsInstaller -eq [bool]$WindowsInstaller) -and ([bool]$_.SystemComponent -eq [bool]$SystemComponent)
+}
+
+# Post some output if the app is found
+if ($AppFound) {
+	Write-Output "Installed"
+}
+'@
+
+$Apps = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | Select-Object DisplayName, DisplayVersion, Version, WindowsInstaller, SystemComponent, UninstallString, QuietUninstallString, Publisher, URLInfoAbout, InstallLocation, InstallSource, PSPath
+$Apps += (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | Select-Object DisplayName, DisplayVersion, SystemComponent, WindowsInstaller, Version, UninstallString, QuietUninstallString, Publisher, URLInfoAbout, InstallLocation, InstallSource, PSPath
+$SelectedApp = $Apps | Sort-Object DisplayName | Out-GridView -Title "Select Application" -OutputMode Single | Select-Object -First 1
+$WindowsInstaller = if ([bool]$SelectedApp.WindowsInstaller){1}else{0}
+$SystemComponent = if([bool]$SelectedApp.SystemComponent){1}else{0}
+$SelectedApp.DisplayName -match '(?:(\d+)\.)?(?:(\d+)\.)?(?:(\d+)\.\d+)'
+$version = $Matches[0]
+$DisplayNameNew = ($SelectedApp.DisplayName).Replace($version, '*')
+$DisplayNameNew
+$DetectionScript = $DetectionScript.Replace("@AppName", $DisplayNameNew).Replace("@AppVersion", $SelectedApp.DisplayVersion).Replace("@WindowsInstaller", $WindowsInstaller).Replace("@SystemComponent", $SystemComponent)
+$DetectionScript | Out-File -FilePath "$PSScriptRoot\$($SelectedApp.DisplayName).ps1" -Encoding oem -Force
