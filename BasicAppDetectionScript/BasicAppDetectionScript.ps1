2127cfb019760cb5158b39ea856c8c49f073bec0
Andrew Jimenez <3790176+asjimene@users.noreply.github.com>
2023-11-08 14:30:00 -0700

    Update Script to Support SystemComponent

--- BasicAppDetectionScript/BasicAppDetectionScript.ps1
+++ BasicAppDetectionScript/BasicAppDetectionScript.ps1
@@ -1,6 +1,7 @@
 $AppName = "" # DisplayName in Add/Remove Programs
 $AppVersion = "" # DisplayVersion of the App in Add/Remove Programs
 $WindowsInstaller= "" # 1 or 0 | 1 is MSI 0 is EXE
+$SystemComponent= "" # 1 or 0 | 1 is SystemComponent = 1, 0 is SystemComponent does not exist or is 0
 
 # Gather all the apps in the Add/Remove Programs Registry Keys
 $Apps = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\) | Get-ItemProperty | select DisplayName, DisplayVersion, WindowsInstaller
@@ -8,7 +9,7 @@ $Apps += (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVers
 
 # Check is the App DisplayName is found and the version in the registry is greater than or equal to the specified AppVersion
 $AppFound = $Apps | Where-Object {
-	($_.DisplayName -like $AppName) -and ([version]$_.DisplayVersion -ge [version]$AppVersion) -and ($_.WindowsInstaller -eq $WindowsInstaller)
+	($_.DisplayName -like $AppName) -and ([version]$_.DisplayVersion -ge [version]$AppVersion) -and ([bool]$_.WindowsInstaller -eq [bool]$WindowsInstaller) -and ([bool]$_.SystemComponent -eq [bool]$SystemComponent)
 }
 
 # Post some output if the app is found
